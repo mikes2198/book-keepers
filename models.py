92532ac071a4b1e931bf547ab4caa65a4b2519b9
mikes2198 <48929019+mikes2198@users.noreply.github.com>
2020-05-13 05:04:03 -0400

    Update models.py
    Added models for my book feature

--- models.py
+++ models.py
@@ -4,6 +4,23 @@ from werkzeug.security import generate_password_hash, check_password_hash
 db = SQLAlchemy()
 
 
+class myBooks(db.Model):
+  bid= db.Column('bid', db.Integer, primary_key= True)
+  id= db.Column('id', db.Integer, db.ForeignKey('user.id'))
+  code= db.Column('code', db.String(10), db.ForeignKey('Book.code'))
+  name= db.column(db.String(80), nullable= False)
+  author= db.column(db.String(80))
+  books= db.relationship('Book')
+
+def toDict(self):
+  return{
+    "id": self.id,
+    "code": self.code,
+    "name": self.name,
+    "author": self.author
+  }
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
@@ -31,3 +48,19 @@ class User(db.Model):
     # To String method
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+
+class Book(db.Model):
+  code= db.Column(db.String(10), primary_key= True)
+  name= db.Column(db.String(80), nullable= False)
+  author= db.Column(db.String(80), nullable= False)
+
+  def toDict(self):
+    return{
+      "code": self.code,
+      "name": self.name,
+      "author": self.author
+    }
+  
+
+
