408f588448552e79175b97652a15ed1432ac1971
mikes2198 <48929019+mikes2198@users.noreply.github.com>
2020-05-13 05:02:12 -0400

    Update main.py
    Addition of routes for My Books

--- main.py
+++ main.py
@@ -52,7 +52,82 @@ def signup():
   return 'user created' # success
 
 
+@app.route('/myBooks', methods['POST'])
+@jwt_required()
+def create my_Book():
+  data= request.get_json()
+  rec = myBooks(bid=data["bid"], id=current_identity.id,code=data["code"], name=data["name"], author=data["author"])
+  db.session.add(rec)
+  db.session.commit()
+  return data["name"]+" captured", 201 
 
+
+@app.route('/Book', methods=['GET'])
+def get_Books():
+  books= Book.query.all()
+  books= [book.toDict() for book in Books]
+  return json.dumps(books)
+
+@app.route('/myBooks', methods=['GET'])
+@jwt_required()
+def get_my_bookss():
+  queryset = myBooks.query.filter_by(id=current_identity.id).all()
+  if queryset == None:
+    return 'Invalid id or unauthorized'
+  if len(queryset) == 0:
+    return 'No Books stored'
+  book = [book.toDict() for book in queryset]
+  return json.dumps(book)
+
+@app.route('/myBooks/<num>', methods=['GET'])
+@jwt_required()
+def get_my_books(num):
+  num = int(num)
+  queryset = myBooks.query.filter_by(id=current_identity.id).all()
+  if queryset == None:
+    return 'Invalid id or unauthorized'
+  if len(queryset) == 0:
+    return 'No Books stored'
+  if num > len(queryset):
+    return 'Invalid num specified'
+  return json.dumps(queryset[num-1].toDict())
+
+@app.route('/myBooks/<num>', methods=['PUT'])
+@jwt_required()
+def update_my_books(num):
+  num = int(num)
+  queryset = myBooks.query.filter_by(id=current_identity.id).all()
+  if queryset == None:
+    return 'Invalid id or unauthorized'
+  if len(queryset) == 0:
+    return 'No Books stored'
+  if num > len(queryset):
+    return 'Invalid num specified'
+  my_Books = queryset[num - 1]
+  data = request.get_json()
+  if 'name' in data:
+    my_Books.name = data['name']
+  db.session.add(my_Books)
+  db.session.commit()
+  return 'Updated', 201
+
+@app.route('/myBooks/<num>', methods=['DELETE'])
+@jwt_required()
+def delete_my_book(num):
+  num = int(num)
+  queryset = myBooks.query.filter_by(id=current_identity.id).all()
+  if queryset == None:
+    return 'Invalid id or unauthorized'
+  if len(queryset) == 0:
+    return 'No Books stored'
+  if num > len(queryset):
+    return 'Invalid num specified'
+  my_Books = queryset[num - 1]
+  db.session.delete(my_Books) # delete the object
+  db.session.commit()
+  return 'Deleted', 204
+
+@app.route('/')
 
 
 app.run(host='0.0.0.0', port=8080)
